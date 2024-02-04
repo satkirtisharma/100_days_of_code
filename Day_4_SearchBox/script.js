109658670d8d85dd3a54c9cfcd559033f8502d71
satkirtisharma <sksharma5864@gmail.com>
2024-02-04 23:49:50 +0530

    create

--- Day_4_SearchBox/script.js
+++ Day_4_SearchBox/script.js
@@ -1,9 +1,9 @@
-var input=document.querySelector
+var input=document.querySelector("input")
 var data=[
-    {name:"harsh",src:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"sk",src:"https://images.unsplash.com/profile-1445470968232-3b6626618f2f?bg=fff&crop=faces&dpr=2&h=150&w=150&auto=format&fit=crop&q=60&ixlib=rb-4.0.3"},
-    {name:"vasu",src:"https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"vedant",src:"https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"harshita",src:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"lorem ",src:"https://images.unsplash.com/profile-1445470968232-3b6626618f2f?bg=fff&crop=faces&dpr=2&h=150&w=150&auto=format&fit=crop&q=60&ixlib=rb-4.0.3"},
+    {name:"    houcinen     ",src:"https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"ipsum",src:"https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
 ]
 var pers="";
 data.forEach(function(elem){
@@ -18,11 +18,11 @@ data.forEach(function(elem){
 document.querySelector(".people").innerHTML=pers;
 input.addEventListener("input",function(){
     var matching =data.filter(function(e){
-        e.name.startsWith(input.value )   
+       return e.name.startsWith(input.value )   
     })
     var newusers="";
     matching.forEach(function(elem){
-        pers +=`<div class="person">
+        newusers +=`<div class="person">
        <div class="img">
            <img src="${elem.src}"
            alt="">
@@ -30,5 +30,5 @@ input.addEventListener("input",function(){
        <h4> ${elem.name} </h4>
    </div>`;
    })
-   document.querySelector("people").innerHTML=newusers;
+   document.querySelector(".people").innerHTML=newusers;
 })
